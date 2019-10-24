eb2ffac0a58042d35f3b2e1de385d4c823ce5cd1
Mousa <sailormousa@gmail.com>
2019-10-24 18:55:43 +0200

    trans

---
+++ resources/lang/ar/trans.php
@@ -0,0 +1,38 @@
+<?php  
+
+return [
+
+	'next_match' => 'المباراة القادمة',
+	'previous_match' => 'المباراة السابقة',
+	'vs' => 'ضد',
+	'remaining_time' => 'الوقت المتبقي',
+	'day' => 'اليوم',
+	'hour' => 'الساعة',
+	'minute' => 'الدقيقة',
+	'second' => 'الثانية',
+	'see_more' => 'مشاهدة المزيد',
+	'football_matches' => 'مباريات كرة القدم',
+	'matches_results' => 'النتائج والمباريات',
+	'last_match' => 'المباراة السابقة',
+	'later_matches' => 'المباريات اللاحقة',
+	'start' => 'البداية',
+	'result' => 'النتيجة',
+	'ticket' => 'إحجز تذكرة',
+	'main' => 'الرئيسية',
+	'first_team' => 'الفريق اﻻول',
+	'tables' => 'الجداول',
+	'news' => 'اﻻخبار',
+	'tickets' => 'التذاكر',
+	'multimedia' => 'الميديا',
+	'about_alraed' => 'عن الرائد',
+	'more_sports' => 'رياضات آخري',
+	'developped' => 'صُمم بواسطة ',
+	'panorama' => 'بانوراما القصيم',
+	'info' => 'معلومات',
+	'address' => 'العنوان',
+	'details' => 'التفاصيل',
+	'alraed' => 'الرائد',
+	'arrow_turn' => 'تستطيع التحريك عن طريق اﻻسهم',
+	'brief_history' => 'نبذة متخصرة عنا',
+	
+];
