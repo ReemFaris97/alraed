1ef30d26fe77f929ca8c8b5f9f9d691cf7066d2e
Memovic <promursi@gmail.com>
2020-09-20 16:43:29 +0200

    en

--- public/site/js/main.js
+++ public/site/js/main.js
@@ -1,75 +1,73 @@
 $(document).ready(function () {
-    //    Wow initialization
-    wow = new WOW(
-    {
-      boxClass:     'wow',      // default
-      animateClass: 'animated', // default
-      offset:       0,          // default
-      mobile:       true,       
-      live:         true        // default
-    }
-  )
-wow.init();
-	
-	
-//	News Slider 
-	if($('#news-slider').length){
-	$('#news-slider').owlCarousel({
-	animateOut: 'slideOutDown',
-	animateIn: 'flipInX',
-	items:1,
-	margin:0,
-	loop:true,
-	stagePadding:0,
-	dots:true,
-	rtl:true,
-	nav:false,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplaySpeed:5000,
-});
+	//    Wow initialization
+	wow = new WOW({
+		boxClass: 'wow', // default
+		animateClass: 'animated', // default
+		offset: 0, // default
+		mobile: true,
+		live: true // default
+	})
+	wow.init();
+
+
+	//	News Slider 
+	if ($('#news-slider').length) {
+		$('#news-slider').owlCarousel({
+			animateOut: 'slideOutDown',
+			animateIn: 'flipInX',
+			items: 1,
+			margin: 0,
+			loop: true,
+			stagePadding: 0,
+			dots: true,
+			rtl: true,
+			nav: false,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			autoplaySpeed: 5000,
+		});
 	}
 	//	News Slider 
-		if($('#multimedia-slider').length){
-	$('#multimedia-slider').owlCarousel({
-    items:1,
-    loop:true,
-    rtl:true,
-    margin:10,
-	dots:false, 
-	nav:true
-});
-		}
-	if($('#partners-slider').length){
-	$('#partners-slider').owlCarousel({
-    loop:true,
-    rtl:true,
-	autoplay:true,
-    margin:10,
-    responsiveClass:true,
-	nav: false,
-	dots: false,
-    responsive:{
-        0:{
-            items:1,
-			margin:0,
-        },
-		400:{
-            items:3,
-			margin:0,
-        },
-        1000:{
-            items:6
-        }
-    }
-})
+	if ($('#multimedia-slider').length) {
+		$('#multimedia-slider').owlCarousel({
+			items: 1,
+			loop: true,
+			rtl: true,
+			margin: 10,
+			dots: false,
+			nav: true
+		});
 	}
-	
-//	Tooltip
-	
-	$('[data-toggle="tooltip"]').tooltip(); 
-// ـJump link
-	  document.querySelectorAll('a.jump').forEach(anchor => {
+	if ($('#partners-slider').length) {
+		$('#partners-slider').owlCarousel({
+			loop: true,
+			rtl: true,
+			autoplay: true,
+			margin: 10,
+			responsiveClass: true,
+			nav: false,
+			dots: false,
+			responsive: {
+				0: {
+					items: 1,
+					margin: 0,
+				},
+				400: {
+					items: 3,
+					margin: 0,
+				},
+				1000: {
+					items: 6
+				}
+			}
+		})
+	}
+
+	//	Tooltip
+
+	$('[data-toggle="tooltip"]').tooltip();
+	// ـJump link
+	document.querySelectorAll('a.jump').forEach(anchor => {
 		anchor.addEventListener('click', function (e) {
 			e.preventDefault();
 			document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -78,27 +76,27 @@ wow.init();
 		});
 	});
 
-//*************************************************
-//    For playing Youtube Videos in fancybox
-    $("a.watch-vid-btn").click(function () {
-        $.fancybox({
-            'padding': 0,
-            'autoScale': false,
-            'transitionIn': 'none',
-            'transitionOut': 'none',
-            'title': this.title,
-            'width': 680,
-            'height': 495,
-            'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-            'type': 'swf',
-            'swf': {
-                'wmode': 'transparent',
-                'allowfullscreen': 'true'
-            }
-        });
+	//*************************************************
+	//    For playing Youtube Videos in fancybox
+	$("a.watch-vid-btn").click(function () {
+		$.fancybox({
+			'padding': 0,
+			'autoScale': false,
+			'transitionIn': 'none',
+			'transitionOut': 'none',
+			'title': this.title,
+			'width': 680,
+			'height': 495,
+			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+			'type': 'swf',
+			'swf': {
+				'wmode': 'transparent',
+				'allowfullscreen': 'true'
+			}
+		});
 
-        return false;
-    });
-	
-	
+		return false;
+	});
+
+
 });
